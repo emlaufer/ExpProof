c3850a7ee8fd78b429c74d1d78e3a25a7cb0efd9
Evan Laufer <evan.m.laufer@gmail.com>
2025-01-12 19:31:27 -0800

    test

--- src/circuit/modules/lime2.rs
+++ src/circuit/modules/lime2.rs
@@ -1736,6 +1736,7 @@ impl LimeCircuit {
         // check dual is feasible
         let mut dual_feasible =
             einsum(config, region, &[inputs.clone(), dual.clone()], "ji,j->i").unwrap();
+        println!("dual feasible: {:?}", dual_feasible.pshow(8));
         for i in 0..2 {
             dual_feasible = crate::circuit::ops::layouts::loop_div(
                 config,
